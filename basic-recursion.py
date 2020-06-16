38e53baa1852123a39e71925726dbb2604770f27
Brandon-Harris <brandonharris177@gmail.com>
2020-06-16 11:54:50 -0600

    quicksort added

--- basic-recursion.py
+++ basic-recursion.py
@@ -1,3 +1,5 @@
+"""countdown"""
+
 def countdown(n):
     if n == 0:
         return
@@ -6,6 +8,8 @@ def countdown(n):
 
 countdown(5)
 
+"""sumlist"""
+
 def sum_list(items):
     if len(items) == 1:
         return items[0]
@@ -13,3 +17,35 @@ def sum_list(items):
         return items[0] + sum_list(items[1:])
 
 sum_list([1, 2, 3, 4, 5])
+
+"""fibonacci"""
+
+def native_fibonacci(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    n_minus1 = native_fibonacci(n-1)
+    n_minus2 = native_fibonacci(n-2)
+    return n_minus1 + n_minus2
+    
+
+"""quicksort"""
+
+def quicksort(data):
+    if len(data) <= 1:
+        return data
+    
+    pivot = data[0]
+
+    left = []
+    right = []
+
+    for current in data[1:]:
+        if current <= pivot:
+            left.append(current)
+        elif current > pivot:
+            right.append(current)
+
+    return quicksort(left) + [pivot] + quicksort(right)
