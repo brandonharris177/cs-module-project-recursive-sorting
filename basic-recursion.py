d54c2962797ae16724924ef40d95ec23895daa57
Brandon-Harris <brandonharris177@gmail.com>
2020-06-16 10:48:04 -0600

    sum_list added

--- basic-recursion.py
+++ basic-recursion.py
@@ -5,3 +5,11 @@ def countdown(n):
     countdown(n-1)
 
 countdown(5)
+
+def sum_list(items):
+    if len(items) == 1:
+        return items[0]
+    else:
+        return items[0] + sum_list(items[1:])
+
+sum_list([1, 2, 3, 4, 5])
